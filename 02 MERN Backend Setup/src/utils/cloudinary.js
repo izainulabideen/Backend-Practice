5ab810ae0569a10a1cc9320c97d11cbaf10e127a
ZainRanaZr <zainrao417@gmail.com>
2024-01-23 23:36:20 +0500

    file handling config multer & cloudinary

---
+++ 02 MERN Backend Setup/src/utils/cloudinary.js
@@ -0,0 +1,26 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET_KEY,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+
+    // Upload file on cloud
+    const response = cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+
+    // file has been uploaded
+    console.log("File uploaded on cloud", (await response).url);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); // remove localy saved temp file as upload operation failed
+    return null;
+  }
+};
